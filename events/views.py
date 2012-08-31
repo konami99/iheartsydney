0a8af043a6d4b8e0450d5296ea8344c746a455b4
Richard Chou <konami99@hotmail.com>
2012-09-01 06:35:58 +1000

    iheartsydney

--- events/views.py
+++ events/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
-import sys
+from xml.dom.minidom import parseString
 from eventfeed import EventFeed
 
 def index(request):
 	ef = EventFeed()
 	feed = ef.get_feed()
+		
 	return render_to_response('index.html', {'feed': feed})
