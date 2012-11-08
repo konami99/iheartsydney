37912f2b244da0a16f781fb69f153148263bcfd9
Richard Chou <konami99@hotmail.com>
2012-11-08 23:49:16 +1100

    view

--- events/views.py
+++ events/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from xml.dom.minidom import parseString
+from xml.dom import minidom
 from eventfeed import EventFeed
+from BeautifulSoup import BeautifulSoup
 import simplejson as json, urllib, pprint
 import feedparser
+import pprint
 
 def index(request):
 	ef = EventFeed()
@@ -19,15 +22,37 @@ def detail(request, namespace):
 	# http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwhatson.cityofsydney.nsw.gov.au%2Fevents%2F9201-pine-streets-term-3-art-workshops%22%20and%20xpath%3D'%2F%2F*%5B%40id%3D%22main%22%5D'&diagnostics=true
 	# http://developer.yahoo.com/python/python-json.html
 	# http://net.tutsplus.com/tutorials/javascript-ajax/how-to-build-an-rss-reader-with-jquery-mobile-2/
+	# //*[@id="main"]/h1
 	
-	# args = {'q':'select * from html where url="http://whatson.cityofsydney.nsw.gov.au/events/' + namespace + '" and xpath=\'//*[@id="main"]\'',
-	#		'diagnostics':'false'
-	#		}
+	args = {'q':'select * from html where url="http://whatson.cityofsydney.nsw.gov.au/events/' + namespace + '" and xpath=\'//*[@id="main"]\''}
 	
-	# url = 'http://query.yahooapis.com/v1/public/yql' + '?' + urllib.urlencode(args)
+	
+	#url = 'http://query.yahooapis.com/v1/public/yql' + '?' + urllib.urlencode(args)
+	
+	#url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwhatson.cityofsydney.nsw.gov.au%2Fevents%2F9201-pine-streets-term-3-art-workshops%22%20and%20xpath%3D'%2F%2F*%5B%40id%3D%22main%22%5D'"
+	
+	
+	
+	
+	url = "http://query.yahooapis.com/v1/public/yql?" + urllib.urlencode(args)
+	
+	
+	# print url
+	
 	# result = feedparser.parse("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwhatson.cityofsydney.nsw.gov.au%2Fevents%2F9201-pine-streets-term-3-art-workshops%22%20and%20xpath%3D'%2F%2F*%5B%40id%3D%22main%22%5D'&diagnostics=false")
 	
-	result = json.load(urllib.urlopen("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwhatson.cityofsydney.nsw.gov.au%2Fevents%2F9201-pine-streets-term-3-art-workshops%22%20and%20xpath%3D'%2F%2F*%5B%40id%3D%22main%22%5D'&format=json"))
-	print result
+	#result = json.load(urllib.urlopen(url))["query"]["results"]
+	#title = result["div"]["h1"]
+	# detail = result["div"]["details"]
+	# pprint.pprint(result["div"]["div"][1][2])
 	
-	return render_to_response('detail.html')
+	xmldoc = BeautifulSoup(urllib.urlopen(url))
+	
+	title = xmldoc.query.results.h1.string
+	des = xmldoc.query.results.findAll("div","description",limit=1)[0].p.string
+	
+	pprint.pprint(des)
+	
+	return render_to_response('detail.html', {'title': title, 'result': des})
+	
+	#return render_to_response('detail.html')
